a80db5a87ef06c1d8b566ad39f0c559e555127f9
duquochuyy <duquochuy2k2@gmail.com>
2023-05-11 11:16:58 +0700

    a

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const expressHandlebars = require('express-handlebars');
+
+app.use(express.static(__dirname + '/html'));
+app.engine('hbs', expressHandlebars.engine({
+    layoutsDir:__dirname + '/views/layouts',
+    defaultLayout: 'layout',
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
+
+app.use('/task1.htm', require('./routes/task1Route'));
+
+
+app.listen(port, () => {
+    console.log(`server is listening on port ${port}`);
+})
---
+++ routes/task1Route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {emotions} = require('../data');
+
+router.get('/', (req, res) => {
+    let quotePath = '/task1/default.jpg';
+    res.render('task1', {emotions, quotePath});
+})
+
+router.get('/:emotion', (req, res) => {
+    let emotion = req.params.emotion;
+    let selectedEmotion = emotions.filter(item => item.title == emotion);
+    let quotePath = '/task1/default.jpg';
+    if (selectedEmotion) {
+        quotePath = selectedEmotion[0].quotePath;
+    }
+    res.render('task1', {emotions, quotePath});
+})
+module.exports = router;
